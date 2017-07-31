c9eb855b5f823491c22784561109e2d28a8e566e
Brannon N. Darby II <bdarby86@gmail.com>
2017-07-31 15:56:50 -0400

    feat(demo): implement setup component

---
+++ demo/src/app/setup/setup.component.ts
@@ -0,0 +1,60 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core'
+import { NgxFormsService } from '../../../../src'
+
+@Component({
+  selector: 'setup',
+  templateUrl: './setup.component.html',
+  styleUrls: ['./setup.component.scss'],
+  // changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SetupComponent {
+
+  importCode = `import { NgxFormsModule } from '@ngx-plus/ngx-forms'
+@NgModule({
+ imports: [
+   NgxFormsModule.forRoot()
+ ]
+})`
+
+  basicCode = '<ngx-form></ngx-form>'
+
+  icon = 'fa fa-magic'
+
+  item = {}
+
+  formConfig = {
+    fields: this.getFormFields(),
+    showCancel: true,
+    hasHeader: false,
+  }
+
+  constructor(
+    private forms: NgxFormsService,
+  ) {
+
+  }
+
+  getFormFields() {
+    const fields = [
+      this.forms.input('firstName', {
+        label: 'First name',
+        placeholder: 'First name',
+      }),
+      this.forms.input('lastName', {
+        label: 'Last name',
+        placeholder: 'Last name',
+      }),
+      this.forms.email('email', {
+        label: 'Email address',
+        placeholder: 'Email address',
+      }),
+    ]
+    return fields
+  }
+
+  public getFormConfig() {
+    return {
+    }
+  }
+
+}
