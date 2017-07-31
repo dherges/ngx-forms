e3b4f96ab3cc79392c3b727e05890c8f7562fc38
Brannon N. Darby II <bdarby86@gmail.com>
2017-07-31 15:57:14 -0400

    feat(demo): implement usage component

--- demo/src/app/usage/usage.component.ts
+++ demo/src/app/usage/usage.component.ts
@@ -1,4 +1,5 @@
-import {Component, ChangeDetectionStrategy} from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core'
+import { NgxFormsService } from '../../../../src'
 
 @Component({
   selector: 'usage',
@@ -8,26 +9,109 @@ import {Component, ChangeDetectionStrategy} from '@angular/core';
 })
 export class UsageComponent {
 
-  totalShare: number = 0;
+  tsCode = `import { NgxFormsService } from '@ngx-plus/ngx-forms'
+@Component({
+ ...
+})
+export class MyComponent {
+  constructor(private forms: NgxFormsService) { }
+  formConfig = {
+    title: 'ngx-forms Example Form',
+    icon: 'fa fa-magic',
+    showCancel: true,
+    hasHeader: true,
+    fields: [
+      this.forms.input('inputField', {
+        label: 'Input',
+        placeholder: 'Input',
+      }),
+      this.forms.email('email', {
+        label: 'Email',
+        placeholder: 'Email',
+      }),
+      this.forms.password('password', {
+        label: 'Password',
+        placeholder: 'Password',
+      }),
+      this.forms.date('date', {
+        label: 'Date',
+        placeholder: 'Date',
+      }),
+      this.forms.textarea('textarea', {
+        label: 'Textarea',
+        placeholder: 'Textarea',
+      }),
+      this.forms.wysiwyg('wysiwyg', {
+        label: 'Wysiwyg',
+        placeholder: 'Wysiwyg',
+      }),
+      this.forms.select('select', {
+        label: 'Select',
+        options: [
+          { label: 1, value: 1 },
+          { label: 2, value: 2 },
+          { label: 3, value: 3 },
+          { label: 4, value: 4 },
+          { label: 5, value: 5 },
+        ]
+      }),
+    ]
+  }
+}`
 
-  sumCounts(count) {
-    this.totalShare += count;
+  templateCode = `
+  <ngx-form [config]="formConfig"
+            [item]="item"
+            (action)="handleAction($event)">
+  </ngx-form>
+  `
+
+  item = {}
+
+  formConfig = {
+    title: 'ngx-forms Example Form',
+    icon: 'fa fa-magic',
+    showCancel: true,
+    hasHeader: true,
+    fields: [
+      this.forms.input('inputField', {
+        label: 'Input',
+        placeholder: 'Input',
+      }),
+      this.forms.email('email', {
+        label: 'Email',
+        placeholder: 'Email',
+      }),
+      this.forms.password('password', {
+        label: 'Password',
+        placeholder: 'Password',
+      }),
+      this.forms.date('date', {
+        label: 'Date',
+        placeholder: 'Date',
+      }),
+      this.forms.textarea('textarea', {
+        label: 'Textarea',
+        placeholder: 'Textarea',
+      }),
+      this.forms.wysiwyg('wysiwyg', {
+        label: 'Wysiwyg',
+        placeholder: 'Wysiwyg',
+      }),
+      this.forms.select('select', {
+        label: 'Select',
+        options: [
+          { label: 1, value: 1 },
+          { label: 2, value: 2 },
+          { label: 3, value: 3 },
+          { label: 4, value: 4 },
+          { label: 5, value: 5 },
+        ]
+      }),
+
+    ]
   }
 
-  importCode = `import { NgxFormsModule } from '@ngx-plus/ngx-forms'
-
-@NgModule({
- imports: [
-   NgxFormsModule.forRoot()
- ]
-})`
-
-  basicCode = ''
-
-  countTemplate = ``;
-
-  countCode = ``;
-
-  countStyle = ``;
+  constructor(private forms: NgxFormsService) { }
 
 }
